4d0c22125ea9bf0391659ea625b79e683d2e3c93
Federico.Murru <djfedemurru@icloud.com>
2024-09-10 13:58:16 +0100

    work

---
+++ app/work/page.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+const workPage = () => {
+	return (
+		<div>
+			<Head>
+				<title>our Work - Health Software Solution</title>
+			</Head>
+			<main className="container mx-auto px-4 py-16">
+				<h1 className="text-3xl font-bold text-blue-600">our Work</h1>
+				<p className="mt-4">Our Work</p>
+			</main>
+		</div>
+	);
+};
+
+export default workPage;
