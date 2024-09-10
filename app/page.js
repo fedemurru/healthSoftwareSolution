42b70edebb46b2cb147b7ed0c84eeeb9b83740d5
Federico.Murru <djfedemurru@icloud.com>
2024-09-10 14:13:01 +0100

    .

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
 						{/* Content */}
 						<div className="flex flex-col space-y-10 lg:mt-16 lg:w-1/2">
 							<h1 className="text-3xl font-semibold text-center lg:text-6xl lg:text-left">
-								Your Ultimate Health Software Solution
+								Your Health Software Solution
 							</h1>
 							<p className="text-lg text-center lg:text-2xl lg:text-left text-gray-600">
 								Experience seamless management of your health with our intuitive
@@ -42,6 +42,41 @@ export default function Home() {
 								height={600}
 								className="relative z-10 lg:top-24 xl:top-0 overflow-x-visible"
 							/>
+						</div>
+					</div>
+				</section>
+				<section id="hero">
+					<div className="container flex flex-col lg:flex-row mx-auto p-6 lg:mb-0 mr-3">
+						{/* Image */}
+						<div className="relative mx-auto lg:mx-0 lg:mb-0 lg:w-1/2 mb-3 ">
+							<Image
+								src="/pexels2.jpg"
+								alt="Hero Illustration"
+								width={1000}
+								height={600}
+								className="relative z-10 lg:top-24 xl:top-0 overflow-x-visible"
+							/>
+						</div>
+
+						{/* Content */}
+						<div className="flex flex-col space-y-10 lg:mt-16 lg:w-1/2 lg:justify-center lg:items-start ml-5">
+							<h1 className="text-3xl font-semibold text-center lg:text-6xl lg:text-left ">
+								How we do it
+							</h1>
+							<p className="text-lg text-center lg:text-2xl lg:text-left text-gray-600">
+								Let’s start with what we don’t do. We don’t ruin your brand by
+								basing your marketing strategy around free appointments or
+								discounts on social media platforms.
+							</p>
+							{/* Buttons Container */}
+							<div className="flex items-center justify-center w-full space-x-4 lg:justify-start">
+								<a
+									href="/about"
+									className="p-4 text-sm font-semibold text-white bg-primary rounded shadow-md border-2 border-gray-300 md:text-base hover:bg-white hover:text-gray-600"
+								>
+									Our Work
+								</a>
+							</div>
 						</div>
 					</div>
 				</section>
