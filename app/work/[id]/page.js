6ae424f8e60534ad9619c2d5cd082174933c816d
Federico.Murru <djfedemurru@icloud.com>
2024-09-10 16:36:07 +0100

    api

--- app/work/[id]/page.js
+++ app/work/[id]/page.js
@@ -3,9 +3,7 @@ import Link from "next/link";
 
 // Fetch project data from the API
 async function getProjectData(id) {
-	const apiUrl =
-		process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/case";
-
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 	try {
 		const res = await fetch(`${apiUrl}/${id}`);
 		if (!res.ok) {
@@ -14,7 +12,7 @@ async function getProjectData(id) {
 		const data = await res.json();
 		return data;
 	} catch (error) {
-		console.error(error);
+		console.error("Error fetching project data:", error);
 		return null;
 	}
 }
