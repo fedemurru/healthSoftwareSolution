54e69559be515485d917eb10f036cc9b37ef4394
Federico.Murru <djfedemurru@icloud.com>
2024-09-10 16:51:07 +0100

    vercel

--- app/work/[id]/page.js
+++ app/work/[id]/page.js
@@ -3,8 +3,7 @@ import Link from "next/link";
 
 // Fetch project data from the API
 async function getProjectData(id) {
-	const apiUrl =
-		process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/case";
+	const apiUrl = process.env.VERCEL_URL || "http://localhost:3000/api/case";
 	try {
 		const res = await fetch(`${apiUrl}/${id}`);
 		if (!res.ok) {
